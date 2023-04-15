cb0d3abd5d5aee414addd7bfd27f0c22a78959e0
MohamedArafath205 <mohamedarafath205@gmail.com>
2023-04-15 19:46:16 +0530

    Resume pagr update

--- src/components/Resumepage.js
+++ src/components/Resumepage.js
@@ -2,13 +2,17 @@ import React from 'react'
 import './Resumepage.css'
 import resume from '../assets/images/Resume(14-4-23).jpg'
 import download from '../assets/images/Resume(14-4-23).pdf'
+import {message} from 'antd';
 
 const Resume = () => {
+  function downloadFile() {
+    message.info('Thanks for downloading', 10);
+  }
   return (
     <div className='hero'>
       <div className='content'>
         <img className="img" src={resume} alt='resume' />
-        <a href={download} className='btn' download="Mohamed Arafath's Resume">Download</a>
+        <a href={download} className='btn' onClick={downloadFile} download="Mohamed Arafath's Resume">Download</a>
       </div>
     </div>
   )
